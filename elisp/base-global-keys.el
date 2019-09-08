06c85afb3ccac34ca19795e0564ef5927668b53c
ncdulo <nick.cardullo@gmail.com>
2019-09-07 22:06:55 -0400

    Add custom functions for enhanced movement and editing

--- elisp/base-global-keys.el
+++ elisp/base-global-keys.el
@@ -6,4 +6,11 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "<RET>") 'newline-and-indent)
 
+
+(global-set-key (kbd "C-a") 'prelude-move-beginning-of-line)
+(global-set-key (kbd "C-c i") 'indent-region-or-buffer)
+(global-set-key (kbd "C-x k") 'kill-default-buffer)
+(global-set-key (kbd "C-o") 'prelude-smart-open-line)
+(global-set-key (kbd "M-o") 'open-line)
+
 (provide 'base-global-keys)
