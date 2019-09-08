a0e309c4ad9e7cd6318773b5a0e2ec1665ad38df
ncdulo <nick.cardullo@gmail.com>
2019-09-07 22:38:15 -0400

    Add ibuffer-vc. Sort buffer list by VC group

--- elisp/base-extensions.el
+++ elisp/base-extensions.el
@@ -96,6 +96,27 @@
   (setq linum-format " %3d ")
   (global-linum-mode nil))
 
+(use-package ibuffer-vc
+  :config
+  (add-hook 'ibuffer-hook
+            (lambda ()
+              (ibuffer-vc-set-filter-groups-by-vc-root)
+              (unless (eq ibuffer-sorting-mode 'alphabetic)
+                (ibuffer-do-sort-by-alphabetic))))
+
+  (setq ibuffer-formats
+        '((mark modified read-only vc-status-mini " "
+                (name 18 18 :left :elide)
+                " "
+                (size 9 -1 :right)
+                " "
+                (mode 16 16 :left :elide)
+                " "
+                (vc-status 16 16 :left)
+                " "
+                filename-and-process)))
+  )
+
 (use-package magit
   :config
 
