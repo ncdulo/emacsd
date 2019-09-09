396548b951014c49546a6a69975541ac91dab0db
ncdulo <nick.cardullo@gmail.com>
2019-09-08 20:05:42 -0400

    Disable underlining of trailing whitespace in cperl-mode

--- elisp/lang-perl.el
+++ elisp/lang-perl.el
@@ -6,4 +6,7 @@
 (add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
 (add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
 
+;; By default cperl-mode underlines trailing whitespace. Disable that.
+(setq cperl-invalid-face nil)
+
 (provide 'lang-perl)
