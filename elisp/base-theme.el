fcd59646f5ef8c91d1b99385f19d01a14c79ffe7
ncdulo <nick.cardullo@gmail.com>
2019-09-11 20:23:13 -0400

    Quick & dirty workaround to emacs 'safe theme?' startup nag

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -5,7 +5,9 @@
 
 (use-package color-theme-sanityinc-tomorrow
   :config
-  (progn
-    (load-theme 'sanityinc-tomorrow-eighties t)))
+  ;; We shouldn't need this, but using as a quick workaround
+  ;; for emacs asking 'is this theme safe?' each start.
+  (setq custom-safe-themes t)
+  (load-theme 'sanityinc-tomorrow-eighties t))
 
 (provide 'base-theme)
