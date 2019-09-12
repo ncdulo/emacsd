7c6b8e04c0d26627a1930a26e6aae812bf9f3dfe
ncdulo <nick.cardullo@gmail.com>
2019-09-11 20:17:21 -0400

    Trying out a new theme -- sanityinc-tomorrow-eighties

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -1,6 +1,11 @@
-(use-package zenburn-theme
-  :defer t
-  :init
-  (load-theme 'zenburn t))
+;; (use-package zenburn-theme
+;;   :defer t
+;;   :init
+;;   (load-theme 'zenburn t))
+
+(use-package color-theme-sanityinc-tomorrow
+  :config
+  (progn
+    (load-theme 'sanityinc-tomorrow-eighties t)))
 
 (provide 'base-theme)
