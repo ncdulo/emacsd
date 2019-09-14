ec4d3a400cae38c5c1c914ffe3a99b0982dfece0
ncdulo <nick.cardullo@gmail.com>
2019-09-14 16:50:38 -0400

    Added new theme, did not change default. Added commentary for themes

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -1,3 +1,8 @@
+;; Add custom themes here. I would like to build a collection of
+;; several to choose from. Maybe a fast theme switcher would be
+;; fun/useful to implement someday
+
+;; Original default zenburn
 ;; (use-package zenburn-theme
 ;;   :defer t
 ;;   :init
@@ -9,5 +14,13 @@
   ;; for emacs asking 'is this theme safe?' each start.
   (setq custom-safe-themes t)
   (load-theme 'sanityinc-tomorrow-eighties t))
+
+;; Not the biggest fan of this theme. It's a bit lighter than
+;; sanityinc-tomorrow and a lot more purple. Keeping the code
+;; in case I would like to use it again sometime.
+;; (use-package apropospriate-theme
+;;   :config
+;;   (setq custom-safe-themes t)
+;;   (load-theme 'apropospriate-dark t))
 
 (provide 'base-theme)
