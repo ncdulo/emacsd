c985e8601024b55790ea453562ff42964eb4ac5c
ncdulo <nick.cardullo@gmail.com>
2019-09-15 09:48:50 -0400

    Tweaked company again. Reorder/regroup of backends

--- elisp/base-extensions.el
+++ elisp/base-extensions.el
@@ -20,7 +20,7 @@
   :diminish " Cmpny"
   :config
   (setq company-minimum-prefix-length 2
-        company-require-match 0
+        company-require-match nil
         company-selection-wrap-around t
         company-show-numbers t
         company-dabbrev-downcase nil
@@ -29,12 +29,14 @@
         company-idle-delay 0.4
         company-begin-commands '(self-insert-command))
   (add-to-list 'company-backends '(company-elisp
-                                   company-files
-                                   company-capf
+                                   company-css
                                    company-cmake
                                    company-nxml
-                                   company-ispell
-                                   company-keywords))
+                                   company-capf
+                                   company-files
+                                   company-keywords
+                                   company-dabbrev-code
+                                   company-dabbrev))
   (add-hook 'after-init-hook 'global-company-mode))
 
 ;;(use-package dashboard
