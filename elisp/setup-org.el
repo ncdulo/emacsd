8fabe3b6fe4bb84ae684f6ae48f650b7786a7194
ncdulo <nick.cardullo@gmail.com>
2019-09-28 19:11:23 -0400

    Added "dev" custom agenda view. Fixed some indentation.

--- elisp/setup-org.el
+++ elisp/setup-org.el
@@ -14,27 +14,27 @@
   ;; Default directories and files
   (setq org-directory "~/org"
         org-default-notes-file (concat org-directory "/notes/notes.org")
-	org-agenda-files (list (concat org-directory "/gtd/inbox.org")
-			       (concat org-directory "/gtd/projects.org")
-			       (concat org-directory "/gtd/tasks.org")
-			       (concat org-directory "/gtd/tickler.org")))
+        org-agenda-files (list (concat org-directory "/gtd/inbox.org")
+                               (concat org-directory "/gtd/projects.org")
+                               (concat org-directory "/gtd/tasks.org")
+                               (concat org-directory "/gtd/tickler.org")))
 
   ;; Capture templates
   (setq org-capture-templates '(("t" "Todo [inbox]" entry
-				 (file+headline "~/org/gtd/inbox.org" "Inbox")
-				 "* TODO %i%?\n%T\n")
-				("T" "Tickler" entry
-				 (file+headline "~/org/gtd/tickler.org" "Tickler")
-				 "* %i%?\n%U\n")
-                ("j" "Journal" entry
-                 (file+datetree "~/org/journal.org")
-                 "* %?\nEntered on %U\n %i\n %a")))
+                                 (file+headline "~/org/gtd/inbox.org" "Inbox")
+                                 "* TODO %i%?\n%T\n")
+                                ("T" "Tickler" entry
+                                 (file+headline "~/org/gtd/tickler.org" "Tickler")
+                                 "* %i%?\n%U\n")
+                                ("j" "Journal" entry
+                                 (file+datetree "~/org/journal.org")
+                                 "* %?\nEntered on %U\n %i\n %a")))
 
   ;; Refile targets
   (setq org-refile-targets '(("~/org/gtd/tasks.org" :maxlevel . 1)
-			     ("~/org/gtd/someday.org" :maxlevel . 1)
-			     ("~/org/gtd/projects.org" :maxlevel . 3)
-			     ("~/org/gtd/tickler.org" :maxlevel . 2)))
+                             ("~/org/gtd/someday.org" :maxlevel . 1)
+                             ("~/org/gtd/projects.org" :maxlevel . 3)
+                             ("~/org/gtd/tickler.org" :maxlevel . 2)))
 
   (setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "APPT(a)"  "WAITING(w!)""|" "DONE(d!)" "CANCELED(c!)")))
 
@@ -43,44 +43,46 @@
   ;; TODO: Projects with next actions.
   ;; TODO: Further refine the custom views to my preference.
   (setq org-agenda-custom-commands
-       ;; View of only tasks with @home or @computer context
-	'(("h" "At home, on computer" ((agenda "" ((org-agenda-span 1)))
-                                   (tags-todo "@home")
-                                   (tags-todo "@computer")))
-      ;; View of only @errand or @work contexts
-      ("e" "Errands, at work" ((agenda "" ((org-agenda-span 1)))
-                               (tags-todo "@errand")
-                               (tags-todo "@work")))
-      ;; View of only project tasks. Maybe we can make this into a tree
-      ;; view, or otherwise separate projects. May also want to limit
-      ;; to only NEXT states. That way an entire project tree can be
-      ;; created, but only NEXT items are displayed. That will make it
-      ;; easier to pick one and dive in. Reduce stress/clutter and
-      ;; help 'get things done'!
-	  ("p" "Project view" tags-todo "project"
-	   ((org-agenda-overriding-header "Projects")))
-      ;; This overview will show a view with:
-      ;;  - Priority A
-      ;;  - Daily agenda
-      ;;  - Normal priority tasks
-	  ("o" "Overview of all tasks"
-       ((tags "PRIORITY=\"A\""
-              ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
-               (org-agenda-overriding-header "High priority unfinished tasks:")))
-        ;; Display agenda for today only
-        (agenda "" ((org-agenda-span 1)
-                    (org-agenda-overriding-header "Today's Agenda")))
-        ;; Filter out any priority A tasks then list the remaining ones.
-        ;; Note -- this seems to skip over priority B and C as well. See if
-        ;; we can't fix that at some point. Alternatively, add more sections
-        ;; or agenda views to cover other priorities.
-        (alltodo ""
-                 ((org-agenda-skip-function
-                   '(or (air-org-skip-subtree-if-priority ?A)
-                        (org-agenda-skip-if nil '(scheduled deadline))))
-                  (org-agenda-overriding-header "All normal priority tasks:")))))
-      ;; Dropped parens to next line for readability. New custom commands go here :)
-      ))
+        ;; View of only tasks with @home or @computer context
+        '(("h" "At home, on computer" ((agenda "" ((org-agenda-span 1)))
+                                       (tags-todo "@home")
+                                       (tags-todo "@computer")))
+          ("d" "Dev tag" ((agenda "" ((org-agenda-span 1)))
+                          (tags-todo "dev")))
+          ;; View of only @errand or @work contexts
+          ("e" "Errands, at work" ((agenda "" ((org-agenda-span 1)))
+                                   (tags-todo "@errand")
+                                   (tags-todo "@work")))
+          ;; View of only project tasks. Maybe we can make this into a tree
+          ;; view, or otherwise separate projects. May also want to limit
+          ;; to only NEXT states. That way an entire project tree can be
+          ;; created, but only NEXT items are displayed. That will make it
+          ;; easier to pick one and dive in. Reduce stress/clutter and
+          ;; help 'get things done'!
+          ("p" "Project view" tags-todo "project"
+           ((org-agenda-overriding-header "Projects")))
+          ;; This overview will show a view with:
+          ;;  - Priority A
+          ;;  - Daily agenda
+          ;;  - Normal priority tasks
+          ("o" "Overview of all tasks"
+           ((tags "PRIORITY=\"A\""
+                  ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                   (org-agenda-overriding-header "High priority unfinished tasks:")))
+            ;; Display agenda for today only
+            (agenda "" ((org-agenda-span 1)
+                        (org-agenda-overriding-header "Today's Agenda")))
+            ;; Filter out any priority A tasks then list the remaining ones.
+            ;; Note -- this seems to skip over priority B and C as well. See if
+            ;; we can't fix that at some point. Alternatively, add more sections
+            ;; or agenda views to cover other priorities.
+            (alltodo ""
+                     ((org-agenda-skip-function
+                       '(or (air-org-skip-subtree-if-priority ?A)
+                            (org-agenda-skip-if nil '(scheduled deadline))))
+                      (org-agenda-overriding-header "All normal priority tasks:")))))
+          ;; Dropped parens to next line for readability. New custom commands go here :)
+          ))
 
   :bind
   ("C-c c" . org-capture)
